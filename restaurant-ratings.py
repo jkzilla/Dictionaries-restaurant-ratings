da8284d3061ded01777d17c323233493223d3f93
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 18:24:07 -0700

    Monday homework dict-1 halfway done

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -4,11 +4,21 @@ our_dictionary_of_ratings = {}
 
 for line in restaurant_ratings:
     restaurant_ratings = line.rstrip()
-    line = restaurant_ratings.split(":")
-    restaurant = line[0]
-    rating = line[1]
-    d = dict([(restaurant,rating)])
-    for d in d.items():
-        print " %s is rated at %s" % d 
+    restaurant, rating = restaurant_ratings.split(":")
     
+    
+    our_dictionary_of_ratings[restaurant] = rating
+# print our_dictionary_of_ratings
 
+print list(our_dictionary_of_ratings)
+
+# for restaurant in sorted(our_dictionary_of_ratings): 
+   
+# sdict = sorted(our_dictionary_of_ratings)
+# print sdict
+#     # print sorted(our_dictionary_of_ratings)
+#     # d = dict(line)
+#     # newd = sorted(d.keys, d.values)
+#     # print newd
+    # # for our_diction in sorted(d):
+    # #     print " %s is rated at %s" % d 
